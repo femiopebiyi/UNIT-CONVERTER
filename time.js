95d5235e3a08324f2c4e58d24e8485bfbdc41bf8
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2023-10-14 23:26:13 +0100

    time conversion seconds

--- time.js
+++ time.js
@@ -43,6 +43,20 @@ function convertTime (){
     } else if (convert.value === 'milliseconds' && converted.value === 'weeks'){
         outcome*= 0.00000000165344
     }
+    // seconds conversion
+    else if (convert.value === 'seconds' && converted.value === 'seconds'){
+        outcome*= 1
+    } else if (convert.value === 'seconds' && converted.value === 'milliseconds'){
+        outcome*= 1000
+    }  else if (convert.value === 'seconds' && converted.value === 'minutes'){
+        outcome*= 0.0166666667
+    }  else if (convert.value === 'seconds' && converted.value === 'hours'){
+        outcome*= 0.0002777778
+    }  else if (convert.value === 'seconds' && converted.value === 'days'){
+        outcome*= 0.0000115741
+    }  else if (convert.value === 'seconds' && converted.value === 'weeks'){
+        outcome*= 0.0000016534
+    }
 
     display = outcome + ' ' + converted.value;
     result.innerText = display
